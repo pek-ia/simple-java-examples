0039844ef1923379ec94ab5a59e828a86a55a447
Paul Kimball <pekimball@interfaceassociates.net>
2023-02-02 14:37:11 -0500

    update ExceptionExample

---
+++ ExceptionExample/src/com/example/except/ExceptionMain.java
@@ -0,0 +1,35 @@
+package com.example.except;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class ExceptionMain {
+
+	static double evalFraction(int num, int den) throws Exception {
+
+	    if (den == 0) 
+         throw new Exception("denominators must not be zero!");
+
+	    return ((double)num)/den;	
+	}
+
+
+	public static void main(String[] args) {
+	    int a = 234, b = 0;
+	    double result = 0.0;
+
+	    try {
+
+	        result = evalFraction(a, b);
+	        System.out.println("result is " + result);
+
+	    } catch (Exception e) {
+
+	        Logger.getGlobal().log( Level.SEVERE, 
+                     "I can't do that math: " + e.getMessage());
+
+	    }		
+	}
+	
+
+}
