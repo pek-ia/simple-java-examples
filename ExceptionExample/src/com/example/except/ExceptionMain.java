e48b2417f32c246e1a1a5a7189e4ae28ee98292c
Paul Kimball <pekimball@interfaceassociates.net>
2023-02-10 15:36:48 -0500

    Fixed it

--- ExceptionExample/src/com/example/except/ExceptionMain.java
+++ ExceptionExample/src/com/example/except/ExceptionMain.java
@@ -7,32 +7,28 @@ public class ExceptionMain {
 
 	static double evalFraction(int num, int den) throws Exception {
 
-	    if (den == 0) 
-         throw new Exception("denominators must not be zero!");
+		if (den == 0)
+			throw new Exception("denominators must not be zero!");
 
-	    return ((double)num)/den;	
+		return ((double) num) / den;
 	}
-
 
 	public static void main(String[] args) {
-	    int a = 239, b = 0;
-	    double result = 0.0;
+		int a = 239, b = 0;
+		double result = 0.0;
 
-	    try {
+		try {
 
-	        result = evalFraction(a, b);
-	        System.out.println("result is " + result);
+			result = evalFraction(a, b);
+			System.out.println("result is " + result);
 
-	    } catch (Exception e) {
+		} catch (Exception e) {
 
-	    	// I'm just putting in a comment.  Somehow my comment is still here.
-	    	
-	        Logger.getGlobal().log( Level.SEVERE, 
+			// I'm just putting in a comment. Somehow my comment is still here.
 
-                     "You CAN Do THAT!!: " + e.getMessage());
+			Logger.getGlobal().log(Level.SEVERE, "You CAN Do THAT!!: " + e.getMessage());
 
-	    }		
+		}
 	}
-	
 
 }
